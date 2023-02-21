44276972b4289f55b09a5319a9b5b52aa0b41eea
Dominika Sobiecka <dominika.sobiecka@gmail.com>
2023-02-21 18:52:01 +0100

    "test": "jest --config jest.config.js",

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-    transform: {
-        '^.+\\.jsx?$': 'babel-jest',
+    testEnvironment: 'jsdom',
+    testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
+    moduleNameMapper: {
+        '\\.(css|less|scss|sss|styl)$': '<rootDir>/node_modules/jest-css-modules',
+        '\\.(gif|ttf|eot|svg)$': '<rootDir>/__mocks__/fileMock.js',
     },
+    setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+    transform: {
+        '^.+\\.js$': 'babel-jest',
+    },
+    transformIgnorePatterns: ['<rootDir>/node_modules/'],
 };
